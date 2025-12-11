22a59d6516caf2cdb79fe6abf8d4b0dd3ca9a106
samiraikramabdeldjelil-dot <samiraikramabdeldjelil@gmail.com>
2025-12-11 03:08:06 +0100

    Update project header with team details

--- project/project.analysis_adn.py
+++ project/project.analysis_adn.py
@@ -1,1 +1,10 @@
-# M1_MCQ_logiciel_adn_analysis_project
+# chef de projet :abdeldjelil samira ikram ,Master 1 Microbiologie et contrôle de Qualité ,11/12/2025
+# liste de  membres :
+# tahraoui hanene
+# dib merwa
+# belamri merwa
+# messaoudi fatima 
+# dali youcef ines
+
+import pandas as pd
+
