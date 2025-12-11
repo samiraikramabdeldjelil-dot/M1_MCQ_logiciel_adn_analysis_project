b054ffcb9871c19b9c81b8750ac66792f1892504
hanene-th <tahraouihanane072@gmail.com>
2025-12-11 12:23:13 +0100

    Add DNA sequences data to analysis script
    Added DNA sequences data with length and GC percentage.

--- project/project.analysis_adn.py
+++ project/project.analysis_adn.py
@@ -8,19 +8,19 @@
 
 import pandas as pd
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Données : Séquences ADN, Longueur , Pourcentage de GC
+data = {
+    "Séquence":["ATGCGTACGTA","GCTAGCTAGGCC", "ATGCGCGTAAGT", "TACGATCGTA", "ATGAAAGGCTT", "CGTACGTAGC", "TTAACCGGAT"]
+    "Longueur": [11, 12, 12, 10, 11, 10, 10],
+    "Pourcentage GC": [50, 66.67, 58.33, 40, 45.45, 60, 50]
+}
+1# Création d'un DataFrame (tableau pandas)
+df = pd.DataFrame(data)
+print("**************** Creattion et affichage ****************,"\n")
+      
+# Affichage du tableau
+print("Tableau des séquences ADN :", "\n")
+print(df, "\n" "\n")
 
 # Opérations sur les tableaux:
 print("************** Opérations **************","\n")
